3a6ba8c34b33487946b66c20b59ac0d349844b18
channguyen <chan.nguyen@mogi.vn>
2020-10-14 13:46:41 +0700

    Updates

---
+++ precache-manifest.c0d1035289bef2ed1c4b99fa229817d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca663b0ebe9c19a90399cb6ec05a274",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "d83b754188dd7977f13a",
+    "url": "/web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "50eac1744f1335f053ad",
+    "url": "/web/static/js/2.96e5e7e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web/static/js/2.96e5e7e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d83b754188dd7977f13a",
+    "url": "/web/static/js/main.df192eb1.chunk.js"
+  },
+  {
+    "revision": "932666a3d42c492ed9a6",
+    "url": "/web/static/js/runtime-main.ac4df23f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web/static/media/logo.5d5d9eef.svg"
+  }
+]);
